60a3e14beee27fa471cdc59aaf14b3777becaceb
SAKIB0004 <mahmudulhaque540@gmail.com>
2026-01-10 00:15:50 +0600

    added folder

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+setup(
+    name="GenerativeAI_Project",
+    version="0.0.0",
+    packages=find_packages(),
+    install_requires=[],
+
+
+    author="Mahmudul Haque Sakib",
+    author_email="mahmudulhaque540@gmail.com"
+)
